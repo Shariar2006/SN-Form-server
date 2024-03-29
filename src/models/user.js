fd1f68c14915d8c92a3e4533a1049bce8edb2278
Shariar2006 <shariarnafis86@gmail.com>
2024-03-29 12:29:36 +0600

    create user schema

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    uEmail: {
+        type: String
+    },
+    uName: {
+        type: String
+    },
+    uPhoto: {
+        type: String,
+        Default: 'no photo'
+    },
+    role: {
+        type: String
+    },
+});
+
+const UserData = model('users', UserSchema);
+
+module.exports = UserData
